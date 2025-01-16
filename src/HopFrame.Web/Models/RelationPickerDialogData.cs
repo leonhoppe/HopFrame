3dde2789550921fde2a8f5eb8f2bffb8c77b978c
Leon Hoppe <leon@ladenbau-hoppe.de>
2025-01-16 16:22:25 +0100

    Added creation/modification confirmation

--- src/HopFrame.Web/Models/RelationPickerDialogData.cs
+++ src/HopFrame.Web/Models/RelationPickerDialogData.cs
@@ -2,7 +2,7 @@
 
 namespace HopFrame.Web.Models;
 
-public class RelationPickerDialogData(TableConfig sourceTable, object? current) {
+public sealed class RelationPickerDialogData(TableConfig sourceTable, object? current) {
     public object? Object { get; set; } = current;
     public TableConfig SourceTable { get; init; } = sourceTable;
 }
